d30e5cc242e707bee74e99ffb1027d1f0b024983
silvaPool <silva.friends22@gmail.com>
2024-01-31 15:20:38 -0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import PrimarySearchAppBar from './components/Header'
 import Title from './components/Title'
 import Cards from './components/Cards'
+import Footer from './components/Footer'
 
 function App() {
   
@@ -24,6 +25,8 @@ function App() {
   <Title />
 
   <Cards />
+
+  <Footer />
       
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function Footer() {
+    return (
+        <Typography variant="h2" sx={{textAlign: 'center'}}>
+            CreepyPastas.com
+        </Typography>
+    )
+}
+
+export default Footer;
