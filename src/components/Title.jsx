45399a4f33fbf4fcd94f6f901e6d51d91f11a075
silvaPool <silva.friends22@gmail.com>
2024-02-22 10:28:32 -0300

    Escolhendo fonts para meu site

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,16 +1,11 @@
 import { SignOutButton, SignedOut } from "@clerk/clerk-react";
 import { Box, Button, Typography } from "@mui/material";
 
-
-
-
 function Title() {
+  function formulario() {
+    window.open("https://form.jotform.com/240523126805651");
+  }
 
-
-    function formulario() {
-      window.open("https://form.jotform.com/240523126805651"); 
-    }
-  
   return (
     <Box
       sx={{
@@ -22,14 +17,19 @@ function Title() {
         padding: "25px",
       }}
     >
-      <Typography variant="h1">CreepyPastas</Typography>
-      <Typography variant="body1">
+      <Typography variant="h1" sx={{fontFamily: "Protest Revolution"}}>CreepyPastas</Typography>
+      <Typography variant="body1" sx={{fontFamily: "Caveat", fontSize: '1.5rem'}}>
         Todas as histórias encontran-se logo abaixo. Tem alguma história e quer
         que todos veem e desfrutem da sua imaginação? Então clique no botão e
         envie a história pra gente!
       </Typography>
       {/* <Typography variant="body1">Divirta-se</Typography> */}
-      <Button onClick={formulario}>Enviar</Button>
+      <button class="learn-more" onClick={formulario}>
+        <span class="circle" aria-hidden="true">
+          <span class="icon arrow"></span>
+        </span>
+        <span class="button-text">Enviar</span>
+      </button>
     </Box>
   );
 }
