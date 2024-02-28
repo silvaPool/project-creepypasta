7bee7ae1858efa8d93815911a3cbc42af107e709
silvaPool <silva.friends22@gmail.com>
2024-02-28 11:31:15 -0300

    Desistalando particles

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -8,7 +8,7 @@ const theme = createTheme({
                 root: {
                     background: 'red',
                     border: '2px solid transparent',
-                    background: 'linear-gradient(#212121, #212121) padding-box,linear-gradient(145deg, transparent 35%,#e81cff, #40c9ff) border-box',
+                    background: 'linear-gradient(#212121, #212121) padding-box,linear-gradient(145deg, transparent 35%,#d3d3d3, #808080) border-box',
                     borderRadius: '8px',
                     transformOrigin: 'right bottom',
                     transition: 'all 0.6s cubic-bezier(0.23, 1, 0.320, 1)',
