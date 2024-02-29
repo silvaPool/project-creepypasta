58fb589f6b6b82770b7c1489518a15232b461dd3
silvaPool <silva.friends22@gmail.com>
2024-02-29 10:56:47 -0300

    Testando os plugins do cloudinary

--- src/services/Upload.jsx
+++ src/services/Upload.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import {Cloudinary} from "@cloudinary/url-gen";
 import {AdvancedImage} from '@cloudinary/react';
-import {fill} from "@cloudinary/url-gen/actions/resize";
+import {fill, scale} from "@cloudinary/url-gen/actions/resize";
+import { cartoonify, outline } from '@cloudinary/url-gen/actions/effect';
+import { max } from '@cloudinary/url-gen/actions/roundCorners';
+import { outer } from '@cloudinary/url-gen/qualifiers/outlineMode';
 
 const Upload = () => {
     const cld = new Cloudinary({
@@ -10,9 +13,16 @@ const Upload = () => {
         }
     });
 
-    const myImage = cld.image('docs/models');
+    const myImage = cld.image('matheus/perfil');
 
-    myImage.resize(fill().width(250).height(250));
+    myImage
+    .effect(cartoonify())
+    .roundCorners(max())
+    .effect(outline().mode(outer()).width(100).color('lightblue'))
+    .backgroundColor('lightblue')
+    .resize(scale().height(300));
+
+    const myUrl = myImage.toURL();
 
     return (
         <div>
